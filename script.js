2e1fd4f232c1c4de1fc14313dc3e71bfb0aecbe2
Manan Ghanshani <mananghanshani@gmail.com>
2026-03-24 22:38:32 +0530

    done

--- script.js
+++ script.js
@@ -13,9 +13,27 @@ class Particle {
     this.effect = effect;
     this.x = Math.floor(Math.random() * this.effect.width);
     this.y = Math.floor(Math.random() * this.effect.height);
+    this.speedX= Math.random() * 5 - 2.5;
+    this.speedY= Math.random() * 5 - 2.5;
+    this.history = [{x:this.x, y:this.y}];
+    this.maxLength = Math.floor(Math.random() * 1000 - 500)
   }
   draw(context) {
-    context.fillRect(this.x, this.y, 5, 5);
+    context.fillRect(this.x, this.y, 10, 10);
+    context.beginPath();
+    context.moveTo(this.history[0].x,this.history[0].y);
+    for ( let i =0; i < this.history.length; i++){
+      context.lineTo(this.history[i].x,this.history[i].y)
+    }
+    context.stroke();
+  }
+  update(){
+    this.x += this.speedX + Math.random() * 15 - 7.5;
+    this.y += this.speedY + Math.random() * 15 - 7.5;
+    this.history.push({x: this.x, y:this.y});
+    if(this.history.length > this.maxLength){
+      this.history.shift();
+    }
   }
 }
 
@@ -23,8 +41,10 @@ class Effect {
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.particles = [];s
+    this.particles = [];
     this.numberOfParticles = 500;
+    this.init();
+
   }
   init() {
     for (let i = 0; i < this.numberOfParticles; i++) {
@@ -34,10 +54,15 @@ class Effect {
   render(context) {
     this.particles.forEach((particle) => {
       particle.draw(context);
+      particle.update();
     });
   }
 }
 const effect = new Effect(canvas.width, canvas.height);
-effect.init();
-effect.render(ctx);
-console.log(effect);
+
+function animate(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  effect.render(ctx);
+  requestAnimationFrame(animate);  
+}
+animate();
