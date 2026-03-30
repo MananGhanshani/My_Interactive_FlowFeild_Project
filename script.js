d4ba2a5bc34b278f9f1ece36c4df8f7e86c00fda
Manan Ghanshani <mananghanshani@gmail.com>
2026-03-30 15:03:54 +0530

    done

--- script.js
+++ script.js
@@ -16,9 +16,10 @@ class Particle {
     this.speedX = Math.random() * 5 - 2.5;
     this.speedY = Math.random() * 5 - 2.5;
     this.history = [{ x: this.x, y: this.y }];
-    this.maxLength = Math.floor(Math.random() * 1000 - 500);
+    this.maxLength = Math.floor(Math.random() * 200 + 10);
     this.angle = 0;
     this.timer = this.maxLength * 2;
+    this.speedModifier = Math.floor(Math.random() * 5 + 1);
   }
   draw(context) {
     context.fillRect(this.x, this.y, 10, 10);
@@ -32,18 +33,31 @@ class Particle {
   update() {
     this.timer--;
     if (this.timer >= 1) {
-      let x = Math.floor(this.x / this.effect.cell);
-    }
+      let x = Math.floor(this.x / this.effect.cellSize);
+      let y = Math.floor(this.y / this.effect.cellSize);
+      let index = y * this.effect.cols + x;
+      this.angle = this.effect.flowFeild[index];
 
-    if (this.history.length > this.maxLength) {
+      this.speedX = Math.cos(this.angle);
+      this.speedY = Math.sin(this.angle);
+      this.x += this.speedX * this.speedModifier;
+      this.y += this.speedY * this.speedModifier;
+
+      this.history.push({ x: this.x, y: this.y });
+      if (this.history.length > this.maxLength) {
+        this.history.shift();
+      }
+    } else if (this.history.length > 1) {
       this.history.shift();
+    } else {
+      this.reset();
     }
   }
-  reset(){
+  reset() {
     this.x = Math.floor(Math.random() * this.effect.width);
     this.y = Math.floor(Math.random() * this.effect.height);
     this.history = [{ x: this.x, y: this.y }];
-    this.timer =  this.maxLength * 2;
+    this.timer = this.maxLength * 2;
   }
 }
 
@@ -53,13 +67,13 @@ class Effect {
     this.height = height;
     this.particles = [];
     this.numberOfParticles = 500;
-    this.init();
 
     this.cellSize = 20;
     this.rows;
     this.cols;
     this.curve = 0.5;
     this.zoom = 0.14;
+    this.init();
   }
   init() {
     this.rows = Math.floor(this.height / this.cellSize);
