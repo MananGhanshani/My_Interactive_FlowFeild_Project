16deb5f0ceb886df84a153b42f092a1bedfd4a22
Manan Ghanshani <mananghanshani@gmail.com>
2026-03-30 14:48:58 +0530

    branch2

--- script.js
+++ script.js
@@ -13,27 +13,37 @@ class Particle {
     this.effect = effect;
     this.x = Math.floor(Math.random() * this.effect.width);
     this.y = Math.floor(Math.random() * this.effect.height);
-    this.speedX= Math.random() * 5 - 2.5;
-    this.speedY= Math.random() * 5 - 2.5;
-    this.history = [{x:this.x, y:this.y}];
-    this.maxLength = Math.floor(Math.random() * 1000 - 500)
+    this.speedX = Math.random() * 5 - 2.5;
+    this.speedY = Math.random() * 5 - 2.5;
+    this.history = [{ x: this.x, y: this.y }];
+    this.maxLength = Math.floor(Math.random() * 1000 - 500);
+    this.angle = 0;
+    this.timer = this.maxLength * 2;
   }
   draw(context) {
     context.fillRect(this.x, this.y, 10, 10);
     context.beginPath();
-    context.moveTo(this.history[0].x,this.history[0].y);
-    for ( let i =0; i < this.history.length; i++){
-      context.lineTo(this.history[i].x,this.history[i].y)
+    context.moveTo(this.history[0].x, this.history[0].y);
+    for (let i = 0; i < this.history.length; i++) {
+      context.lineTo(this.history[i].x, this.history[i].y);
     }
     context.stroke();
   }
-  update(){
-    this.x += this.speedX + Math.random() * 15 - 7.5;
-    this.y += this.speedY + Math.random() * 15 - 7.5;
-    this.history.push({x: this.x, y:this.y});
-    if(this.history.length > this.maxLength){
+  update() {
+    this.timer--;
+    if (this.timer >= 1) {
+      let x = Math.floor(this.x / this.effect.cell);
+    }
+
+    if (this.history.length > this.maxLength) {
       this.history.shift();
     }
+  }
+  reset(){
+    this.x = Math.floor(Math.random() * this.effect.width);
+    this.y = Math.floor(Math.random() * this.effect.height);
+    this.history = [{ x: this.x, y: this.y }];
+    this.timer =  this.maxLength * 2;
   }
 }
 
@@ -45,8 +55,23 @@ class Effect {
     this.numberOfParticles = 500;
     this.init();
 
+    this.cellSize = 20;
+    this.rows;
+    this.cols;
+    this.curve = 0.5;
+    this.zoom = 0.14;
   }
   init() {
+    this.rows = Math.floor(this.height / this.cellSize);
+    this.cols = Math.floor(this.width / this.cellSize);
+    this.flowFeild = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let angle = (Math.cos(x * this.zoom) + Math.sin(y * this.zoom)) * this.curve;
+        this.flowFeild.push(angle);
+      }
+    }
+
     for (let i = 0; i < this.numberOfParticles; i++) {
       this.particles.push(new Particle(this));
     }
@@ -60,9 +85,9 @@ class Effect {
 }
 const effect = new Effect(canvas.width, canvas.height);
 
-function animate(){
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   effect.render(ctx);
-  requestAnimationFrame(animate);  
+  requestAnimationFrame(animate);
 }
 animate();
